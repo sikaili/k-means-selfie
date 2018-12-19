7b8b367909ff23e862d44abaf101288f2f2809df
sikai <skyl@me.com>
2018-12-19 11:59:27 +0100

    responsive_add_video

--- settings.js
+++ settings.js
@@ -1,18 +1,41 @@
-function touchStarted(){
-  state=1;
+function touchStarted() {
+  // noLoop();
+
+  // state = 1;
+  // points = [];
+  // video.loadPixels();
+  // loadPixels();
+  // for (let y = 0; y < video.height; y++) {
+  //   for (let x = 0; x < video.width; x++) {
+  //     let index = (video.width - x + 1 + (y * video.width)) * 4;
+  //     let r = video.pixels[index + 0];
+  //     let g = video.pixels[index + 1];
+  //     let b = video.pixels[index + 2];
+  //     let bright = (r + g + b) / 3;
+  //     let ee = createVector(x * vScale, y * vScale);
+  //     points.push(ee);
+  //   }
+  // }
+  // kMoyen = [...points].slice(0, k);
+
+  // for (let i = 0; i < kMoyen.length; i++) {
+  //   kPoints[i] = [];
+  //   colors[i] = [random(0, 255), random(0, 255), random(0, 255), 180];
+  // }
 }
-function touchEnded(){
-  state=0;
-  if(state==-2){
-    if(mouseX<width/2){
-      state =0;
-      rrr = Math.floor(Math.random()*100);
-    }
-    else{
-      state =-1;
-      for(var e=0;e<mm[drawsN].length;e++){
-        var dra = new Draw(e,mm[drawsN][e]);
-        draws[e]= dra;
+
+function touchEnded() {
+  // loop();
+  state = 0;
+  if (state == -2) {
+    if (mouseX < width / 2) {
+      state = 0;
+      rrr = Math.floor(Math.random() * 100);
+    } else {
+      state = -1;
+      for (var e = 0; e < mm[drawsN].length; e++) {
+        var dra = new Draw(e, mm[drawsN][e]);
+        draws[e] = dra;
       }
     }
   }
@@ -33,58 +56,52 @@ function touchEnded(){
   } else {
     doubleClick = false;
   }
-  if(doubleClick){
-  }
+  if (doubleClick) {}
 
 }
 
-function keyPressed(){
+function keyPressed() {
 
-  if(keyCode == 189 || keyCode == 187){
-  }
+  if (keyCode == 189 || keyCode == 187) {}
 
 }
 
 
-function addSnapshot( id )
-{
+function addSnapshot(id) {
   var dumps = [];
-      // for(var mm =0; mm<draws.length; mm++){
-      //   var dump = draws[mm].mp.map( function( element )
-      //   {
-      //       return { x : element.x , y : element.y }
-      //   })
-      //   dumps.push(dump);
-      // }
-      
-      console.log(dumps);
-      
-      localStorage.setItem( "canvas-" + id , JSON.stringify(dumps) )
-      
-}     
+  // for(var mm =0; mm<draws.length; mm++){
+  //   var dump = draws[mm].mp.map( function( element )
+  //   {
+  //       return { x : element.x , y : element.y }
+  //   })
+  //   dumps.push(dump);
+  // }
 
-function removeSnapshot(id) 
-{
-  localStorage.removeItem("canvas-" + id) ;
-}  
+  console.log(dumps);
+
+  localStorage.setItem("canvas-" + id, JSON.stringify(dumps))
+
+}
+
+function removeSnapshot(id) {
+  localStorage.removeItem("canvas-" + id);
+}
 
 
-function getSnapshot( id )
-{
-  var canvas = JSON.parse(localStorage.getItem( "canvas-" + id )) ;
+function getSnapshot(id) {
+  var canvas = JSON.parse(localStorage.getItem("canvas-" + id));
   // var canvas = JSON.parse(myJSON);
-  return canvas ;
-}  
+  return canvas;
+}
 
-function resetAllSnapshots() 
-{
-  localStorage.clear() ;
+function resetAllSnapshots() {
+  localStorage.clear();
 }
 
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight)
   background(50, 50, 200)
-  link1.position(0.92 * windowWidth - 60, 0.9 * windowHeight+17);
+  link1.position(0.92 * windowWidth - 60, 0.9 * windowHeight + 17);
   link.position(0.92 * windowWidth - 60, 0.9 * windowHeight);
 }
--- sketch.js
+++ sketch.js
@@ -14,17 +14,21 @@ let colors = [];
 let pixelPos = [];
 let img;
 let count = 0;
+let [video, vScale] = ["", 16];
 
 function preload() {
   img = loadImage("assets/3.jpg"); // Load the image
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  // pixelDensity(1);
+  // video = createCapture(VIDEO);
 
+  createCanvas(windowWidth, windowWidth * 0.7 > windowHeight ? windowHeight : windowWidth * 0.7);
+  // video.size(width / vScale, height / vScale);
+  // count=0, pixelPos=[];
   img.loadPixels();
   img.resize(width, height);
-
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
       let index = (width * y + x) * 4;
@@ -34,7 +38,6 @@ function setup() {
       }
     }
   }
-  console.log(count);
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
       let index = (width * y + x) * 4;
@@ -45,24 +48,22 @@ function setup() {
       }
     }
   }
-  console.log(pixelPos);
 
-  // frameRate(24);
   noCursor();
   textSize(30);
 
   for (let i = 0; i < 10000; i++) {
-
-    // let dump = createVector(random(noise(i / 10000) * width, noise(2 - i / 3000) * height));
     let dump = createVector(random(0, width), random(0, height));
     points.push(dump);
   }
 
   points = pixelPos;
+  // points = [];
   kMoyen = [...points].slice(0, k);
-  // for (let i = 0; i < k; i++) {
-  //   kMoyen[k] = createVector(random(width / 2, width / 2.1), random(width / 2, width / 2.1));
-  // }
+
+  for (let i = 0; i < k; i++) {
+    kMoyen[k] = createVector(random(width / 2, width / 2.1), random(width / 2, width / 2.1));
+  }
   for (let i = 0; i < kMoyen.length; i++) {
     kPoints[i] = [];
     colors[i] = [random(0, 255), random(0, 255), random(0, 255), 180];
@@ -70,7 +71,6 @@ function setup() {
 }
 
 function aver(arr) {
-
   let [xs, ys] = [
     [],
     []
@@ -81,63 +81,41 @@ function aver(arr) {
   })
   let x = xs.reduce((a, b) => a + b) / xs.length;
   let y = ys.reduce((a, b) => a + b) / ys.length;
-  // console.log(x);
   return createVector(x, y);
 }
 
 function draw() {
-  // pixelDensity(1.0)
   background(0);
+  // text
   noStroke();
   textAlign(CENTER);
   fill(255);
   text("K-Moyennes", width / 2, height * 0.8)
-
-
   for (let i = 0; i < points.length; i++) {
     for (let t = 0; t < kPoints.length; t++) {
       dis[t] = p5.Vector.dist(points[i], kMoyen[t]);
     }
     let t = Math.min(...dis);
     let n = dis.indexOf(t);
-    // console.log(n);
     kPoints[n].push(points[i]);
-    // let dis0 = p5.Vector.dist(points[i], kMoyen[0]);
-    // let dis1 = p5.Vector.dist(points[i], kMoyen[1]);
-    // let dis2 = p5.Vector.dist(points[i], kMoyen[2]);
-    // (dis0 < dis1) && (dis0 < dis2) ? k0.push(points[i]): "";
-    // (dis1 < dis2) && (dis1 < dis0) ? k1.push(points[i]): "";
-    // (dis2 < dis1) && (dis2 < dis0) ? k2.push(points[i]): "";
   }
   for (let i = 0; i < kPoints.length; i++) {
     kMoyen[i] = aver(kPoints[i]);
   }
-  // kMoyen[0] = aver(k0);
-  // kMoyen[1] = aver(k1);
-  // kMoyen[2] = aver(k2);
-  // kPoints[0].forEach(a => {
-  //   fill(255, 0, 0, 180);
-  //   ellipse(a.x, a.y, 10, 10)
-  // })
-  // ellipse(kMoyen[0].x, kMoyen[0].y, k0.length / 3);
-
+  // display
   for (let i = 0; i < kPoints.length; i++) {
     fill(colors[i]);
     kPoints[i].forEach(a => {
       ellipse(a.x + Math.random(), a.y - Math.random(), 2, 2)
     })
     fill(colors[i][0], colors[i][1], colors[i][2], 100);
-
-    ellipse(kMoyen[i].x, kMoyen[i].y, kPoints[i].length / 3 / k * 15);
+    ellipse(kMoyen[i].x, kMoyen[i].y, Math.sqrt(kPoints[i].length) / k * 60);
   }
 
 
-  for (let i = 0; i < kPoints.length; i++) {
+  for (let i = 0; i < kMoyen.length; i++) {
     kPoints[i] = [];
   }
-  // console.log(kPoints);
-  // console.log(kMoyen);
-
 }
 
 
