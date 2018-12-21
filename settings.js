c661855bf01da1c8437be9b2dec0d71ed43e99d6
sikai <skyl@me.com>
2018-12-21 15:59:33 +0800

    try_BnW_line_stroke

--- settings.js
+++ settings.js
@@ -12,14 +12,12 @@ function touchEnded() {
   } else {
     kMoyen = points.slice(0, k + 1);
   }
-  for (let i = 0; i < kMoyen.length; i++) {
-    kPoints[i] = [];
-    colors[i] = [random(100, 500), random(50, 255), random(100, 555), 200];
-  }
   // rest points for each center and set Color
   for (let i = 0; i < kMoyen.length; i++) {
     kPoints[i] = [];
-    colors[i] = [random(100, 500), random(50, 255), random(100, 555), 200];
+    // colors[i] = [random(100, 500), random(50, 255), random(100, 555), 200];
+    let t = random(0, 255);
+    colors[i] = [t, t, t, 200];
   }
 }
 
--- sketch.js
+++ sketch.js
@@ -75,7 +75,7 @@ function setup() {
   }
   for (let i = 0; i < kMoyen.length + 1; i++) {
     kPoints[i] = [];
-    colors[i] = [random(100, 255), random(50, 255), random(100, 255), 200];
+    colors[i] = [random(100, 255), random(50, 255), random(100, 500), 200];
   }
 }
 
@@ -153,8 +153,11 @@ function draw() {
   // display
   for (let i = 0; i < kPoints.length; i++) {
     fill(colors[i]);
+    stroke(colors[i]);
     kPoints[i].forEach(a => {
-      ellipse(a.x + Math.random(), a.y - Math.random(), height > width ? 4 : 2);
+      // ellipse(a.x + Math.random(), a.y - Math.random(), height > width ? 4 : 2);
+      line(a.x + Math.random(), a.y - Math.random(), a.x + 4, a.y - 4);
+
     })
     fill(colors[i][0], colors[i][1], colors[i][2], 100);
     i === kMoyen.length - 1 ? fill(200, 0, 180, 200) : "";
